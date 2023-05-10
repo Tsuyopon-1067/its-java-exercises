621b6ade04fca5a923f9ea7b4ba9a8c3a8e6ca6d
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-05-11 08:41:50 +0900

    "day1"

---
+++ day1/Main1.java
@@ -0,0 +1,16 @@
+package day1;
+
+class Main1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("ABC");
+        System.out.println("----------");
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.println("----------");
+        System.out.print("A");
+        System.out.print("B");
+        System.out.print("C");
+    }
+}
